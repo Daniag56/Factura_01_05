943bbb2aaa988a5db15ad32dc5cc4238d0765bf5
Daniel A.García <dnlrsgrc@gmail.com>
2025-09-25 11:10:16 +0200

    feat:Factura

---
+++ src/Factura.java
@@ -0,0 +1,14 @@
+public class  Factura{
+    public static void main(String[] args) throws Exception {
+        System.out.println("Rojo Polo Paella Inc.");
+        System.out.println("Carretera muelle 38");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.printf("%-30s%-30s%-30s%30s \n", "FACTURAR A", "ENVIAR A","Nº DE FACTURA","ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s \n", "FACTURAR A", "ENVIAR A","Nº DE FACTURA","ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s \n", "FACTURAR A", "ENVIAR A","Nº DE FACTURA","ES-001");
+        System.out.printf("%-30s%-30s%-30s%30s","","", "FECHA VICTO \n", "29-01-2019");
+
+    }
+}
